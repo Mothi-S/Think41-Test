a633107d91e8fb339377fba791d6ac3eda9ac386
Mothi-S <159180250+Mothi-S@users.noreply.github.com>
2025-07-31 14:24:04 +0530

    Delete config directory

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('think41_db', 'postgres', 'Admin', {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: false
-});
-
-module.exports = sequelize;
