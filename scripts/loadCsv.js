e1f5f69e280e579b94b72b3cffe3e508e6c2c28c
Mothi S <mothi3302@gmail.com>
2025-07-31 11:11:57 +0530

    Milestone 2 REST API for products

--- scripts/loadCsv.js
+++ scripts/loadCsv.js
@@ -1,7 +1,57 @@
+// const fs = require('fs');
+// const csv = require('csv-parser');
+// const Product = require('../models/product');
+// const sequelize = require('../config/db');
+
+// async function loadCSV() {
+//   try {
+//     await sequelize.sync({ force: true });
+//     console.log('Database synced.');
+
+//     const products = [];
+
+//     fs.createReadStream('./data/products.csv')
+//       .pipe(csv())
+//       .on('data', (row) => {
+//         console.log(row);
+//         products.push({
+//   id: parseInt(row.id),
+//   cost: parseFloat(row.cost),
+//   category: row.category,
+//   product_name: row.name,          // ✅ 'name' is the CSV header
+//   brand: row.brand,
+//   price: parseFloat(row.retail_price),
+//   department: row.department,
+//   sku: row.sku,
+//   distribution_center_id: parseInt(row.distribution_center_id)
+// });
+
+//         // products.push({
+//         //   id: parseInt(row.id),
+//         //   product_name: row.product_name,
+//         //   department: row.department,
+//         //   price: parseFloat(row.price),
+//         //   image: row.image
+//         // });
+//       })
+//       .on('end', async () => {
+//         await Product.bulkCreate(products);
+//         console.log('CSV data loaded successfully.');
+//         process.exit();
+//       });
+
+//   } catch (err) {
+//     console.error('Error loading CSV:', err);
+//   }
+// }
+
+// loadCSV();
+
+
 const fs = require('fs');
 const csv = require('csv-parser');
-const Product = require('../models/product');
 const sequelize = require('../config/db');
+const Product = require('../models/product');
 
 async function loadCSV() {
   try {
@@ -15,10 +65,14 @@ async function loadCSV() {
       .on('data', (row) => {
         products.push({
           id: parseInt(row.id),
-          product_name: row.product_name,
+          cost: parseFloat(row.cost),
+          category: row.category,
+          name: row.name,
+          brand: row.brand,
+          price: parseFloat(row.retail_price),
           department: row.department,
-          price: parseFloat(row.price),
-          image: row.image
+          sku: row.sku,
+          distribution_center_id: parseInt(row.distribution_center_id)
         });
       })
       .on('end', async () => {
@@ -27,8 +81,8 @@ async function loadCSV() {
         process.exit();
       });
 
-  } catch (err) {
-    console.error('Error loading CSV:', err);
+  } catch (error) {
+    console.error('Error loading CSV:', error);
   }
 }
 
